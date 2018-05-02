b1f51f7c576852e38928967b8529be500d1f0d46
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-02 13:29:07 +0800

    init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,31 @@
+<!-- 首页组件 -->
+<template>
+<div>
+    <home-header></home-header>
+</div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+
+export default {
+    
+name:'Home',
+  data () {
+    return {
+    };
+  },
+
+  components: {
+      HomeHeader,
+  },
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+</style>
