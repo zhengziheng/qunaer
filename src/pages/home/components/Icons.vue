faaefb793a43a8c8c4566bbf1f89589d4417f402
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-03 09:25:20 +0800

    实现swiper效果

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,110 +1,137 @@
 <!--首页图标导航组件  -->
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png" alt="">
-      </div>
-      <p class="icon-desc">必游榜单</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png" alt="">
-      </div>
-      <p class="icon-desc">踏青赏花</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png" alt="">
-      </div>
-      <p class="icon-desc">一日游</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png" alt="">
-      </div>
-      <p class="icon-desc">动植物园</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png" alt="">
-      </div>
-      <p class="icon-desc">故宫</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png" alt="">
-      </div>
-      <p class="icon-desc">古北水镇</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1804/ed/cf572be30fc32f02.png" alt="">
-      </div>
-      <p class="icon-desc">Q+精选</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+        <p class="icon-desc">{{item.desc}}</p>
+        </div>
+    </swiper-slide>
     
-      icons
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-name:'HomeIcons',
+  name: "HomeIcons",
 
-  data () {
+  data() {
     return {
+      iconList:[
+        {
+          id:'0001',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc:'景点门票',
+        },
+        {
+          id:'0002',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc:'必游帮当',
+        },
+        {
+          id:'0003',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+          desc:'踏青赏花',
+        },
+        {
+          id:'0004',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+          desc:'一日游',
+        },
+        {
+          id:'0005',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc:'动植物园',
+        },
+        {
+          id:'0006',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc:'故宫',
+        },
+        {
+          id:'0007',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+          desc:'古北水镇',
+        },
+        {
+          id:'0008',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/ed/cf572be30fc32f02.png',
+          desc:'Q+精选',
+        },
+        {
+          id:'0009',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc:'演出',
+        },
+        {
+          id:'00010',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'自然风光',
+        },
+      ]
     };
   },
 
   components: {},
 
-  computed: {},
-
+  computed: {
+    pages (){
+      const pages =[]
+      this.iconList.forEach((item,index)=>{
+        const page = Math.floor(index/8)
+        if(!pages[page]){
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  },
 
   methods: {}
-}
-
+};
 </script>
 <style lang='stylus' scoped>
-@import '~styles/varibles.styl'
-.icons
-  width 100%
-  overflow hidden
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl'
+.icons >>> .swiper-container
   height 0
-  padding-bottom 50%//宽度设置为100%，高站位为宽的50%
-  .icon
-    position relative
-    overflow hidden
-    float left
-    height 0
-    width 25%
-    padding-bottom 25%//width 25% paddingbottom 25% 设置图标的宽高为父容器的 25%
-    .icon-img
-      position absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
-      .icon-img-content
-        display block
-        margin 0 auto
-        height 100%
-    .icon-desc
-      position absolute
-      left 0
-      right 0
-      bottom 0
-      height .44rem
-      line-height .44rem
-      color $darkTextColor
-      text-align center 
+  padding-bottom 50%
+  // width: 100%;
+  // overflow: hidden;
+  // height: 0;
+  // padding-bottom: 50%; // 宽度设置为100%，高站位为宽的50%
+.icon 
+  position: relative;
+  overflow: hidden;
+  float: left;
+  height: 0;
+  width: 25%;
+  padding-bottom: 25%; // width 25% paddingbottom 25% 设置图标的宽高为父容器的 25%
+  .icon-img 
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+    .icon-img-content 
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+  .icon-desc 
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    color: $darkTextColor;
+    text-align: center;
+    ellipsis()
 </style>
