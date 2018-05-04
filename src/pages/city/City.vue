fbc2c9b7356ba79780ae61390f6bfd5821608f0a
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-04 09:55:21 +0800

    city-router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,29 @@
+<!-- 城市列表组件 -->
+<template>
+    <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+name:'City',
+
+  data () {
+    return {
+    };
+  },
+
+props:{},
+  components: {
+    CityHeader,
+  },
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+</style>
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,49 @@
+<!--  -->
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+name:'CityHeader',
+
+  data () {
+    return {
+    };
+  },
+
+props:{},
+  components: {},
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.header
+  position relative
+  height .86rem
+  line-height .86rem
+  overflow hidden
+  text-align center
+  color #fff
+  background $bgColor
+  font-size .32rem
+  .header-back
+    position absolute
+    top 0
+    left 0
+    width 0.64rem
+    text-align center 
+    font-size .4rem
+    color #fff
+</style>
