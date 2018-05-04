babbac87a2ceb69690f4c75f95667224ecdec4dd
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-04 08:47:31 +0800

    添加首页数据

--- config/index.js
+++ config/index.js
@@ -7,10 +7,17 @@ const path = require('path')
 module.exports = {
   dev: {
 
-    // Paths
+    // Paths webpack-dev-server 开发环境下api的转发
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target:'http://localhost:8080',
+        pathRewrite:{
+          '^/api':'/static/mock'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
     
 name:'Home',
@@ -31,10 +32,25 @@ name:'Home',
       HomeWeekend,
   },
 
+  mounted(){
+    this.getHomeInfo()
+
+    
+  },
+
   computed: {},
 
 
-  methods: {}
+  methods: {
+    getHomeInfo(){
+      axios.get('/api/index.json')
+      .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc(res){
+      console.log(res);
+      
+    }
+  }
 }
 
 </script>
