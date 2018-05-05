604755de340388391b577e5ef48f21ee9b4f39c4
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-05 13:25:51 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,12 +3,16 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
+      <city-list></city-list>
+      <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
 name:'City',
 
@@ -21,6 +25,8 @@ props:{},
   components: {
     CityHeader,
     CitySearch,
+    CityList,
+    CityAlphabet,
   },
 
   computed: {},
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,49 @@
+<!-- 字母表组件 -->
+<template>
+  <div class="list">
+    <span class="item">A</span>
+    <span class="item">A</span>
+    <span class="item">A</span>
+    <span class="item">A</span>
+    <span class="item">A</span>
+    <span class="item">A</span>
+    <span class="item">A</span>
+  </div>
+</template>
+
+<script>
+export default {
+name:'CityAlphabet',
+
+  data () {
+    return {
+    };
+  },
+
+props:{},
+  components: {},
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.list 
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  right 0
+  top 1.58rem
+  bottom 0
+  width .4rem
+  // background red
+  .item
+    text-align center
+    line-height .44rem
+    color $bgColor
+</style>
