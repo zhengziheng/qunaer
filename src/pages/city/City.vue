938e3d036a0c709e455ce9008b7509bc20280f3c
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-06 12:17:46 +0800

    search-finished

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <city-header></city-header>
-      <city-search></city-search>
+      <city-search :cities="cities"></city-search>
       <city-list 
       :cities="cities" 
       :hotCities="hotCities"
