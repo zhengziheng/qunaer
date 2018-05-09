cefc6eac369ab426497062b5e07b6740b2f11b57
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-09 17:34:08 +0800

    banner-finish

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,32 @@
+<!-- 详情页组件 -->
+<template>
+  <div>
+    <detail-banner></detail-banner>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+export default {
+
+name:'Detail',
+
+  data () {
+    return {
+    };
+  },
+
+props:{},
+  components: {
+    DetailBanner,
+  },
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,81 @@
+<!-- 详情封面 -->
+<template>
+<div>
+  <div class="banner" @click="handleBannerClick">
+    <img src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="" class="banner-img">
+    <div class="banner-info">
+      <div class="banner-title">故宫(AAAAA景区)</div>
+      <div class="banner-number"><span class="banner-icon">&#xe61b;</span>39</div>
+    </div>
+  </div>
+  <common-gallary 
+  :imgs="imgs" 
+  v-show="showGallary"
+  @close="handleGallaryClose"
+  ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+name:'DetailBanner',
+
+  data () {
+    return {
+      imgs:['http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_r_800x800_6edd8174.jpg','http://img1.qunarzz.com/sight/p55/201211/04/fbcab3e5d6479ce893835fbb.jpg_r_800x800_6360f514.jpg'],
+      showGallary:false,
+    };
+  },
+
+props:{},
+  components: {
+    CommonGallary,
+  },
+
+  computed: {},
+
+
+  methods: {
+    handleBannerClick(){
+      this.showGallary = true
+    },
+    handleGallaryClose(){
+      this.showGallary = false
+    }
+  }
+}
+
+</script>
+<style lang='stylus' scoped>
+.banner
+  position relative
+  overflow hidden
+  height 0
+  padding-bottom 55.5%
+  .banner-img
+    width 100%
+    
+  .banner-info
+    display flex
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    line-height .6rem
+    color #fff
+    background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+    .banner-title
+      flex 1
+      font-size .32rem
+      padding 0 .2rem
+    .banner-number
+      margin-top .2rem
+      padding 0 .4rem
+      line-height .32rem
+      height .32rem
+      border-radius .2rem
+      font-size .24rem
+      background rgba(0,0,0,0.8)
+
+</style>
