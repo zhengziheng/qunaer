612bb6d0752e0de240517e32a5fa6c2fdab4399f
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-05-10 21:37:48 +0800

    animation finished

---
+++ src/common/fade/Fade.vue
@@ -0,0 +1,32 @@
+<!--渐隐动画组件  -->
+<template>
+  <transition>
+  <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+name:'Fade',
+
+  data () {
+    return {
+    };
+  },
+
+props:{},
+  components: {},
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+.v-enter,.v-leave-to
+  opacity :0
+.v-enter-active,.v-leave-active
+  transition opacity .5s
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -8,16 +8,19 @@
       <div class="banner-number"><span class="banner-icon">&#xe61b;</span>{{this.bannerImgs.length}}</div>
     </div>
   </div>
+  <fade-animation>
   <common-gallary 
   :imgs="bannerImgs" 
   v-show="showGallary"
   @close="handleGallaryClose"
   ></common-gallary>
+  </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/Fade'
 export default {
 name:'DetailBanner',
 
@@ -34,6 +37,7 @@ props:{
 },
   components: {
     CommonGallary,
+    FadeAnimation,
   },
 
   computed: {},
